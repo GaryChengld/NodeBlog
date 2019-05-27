50b116b723a56938f2545b44dbd28e36bf060935
Gary Cheng <chengld@yahoo.com>
2019-05-27 15:53:18 -0400

    Update postsService.js

--- api/posts/postsService.js
+++ api/posts/postsService.js
@@ -11,6 +11,33 @@ const create = (post) => {
     return Post.create(post);
 }
 
+const update = (id, post) => {
+    return new Promise((resolve, reject) => {
+        Post.findById(id)
+            .select('-comments')
+            .exec((err, result) => {
+                if (err) {
+                    reject(err);
+                } else if (!result) {
+                    resolve(null);
+                } else {
+                    result.author = post.author;
+                    result.title = post.title;
+                    result.body = post.body;
+                    result.tags = post.tags;
+                    result.updatedOn = new Date();
+                    result.save((err, savedPost) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(savedPost);
+                        }
+                    })
+                }
+            })
+    });
+}
+
 module.exports = {
-    findById, create
+    findById, create, update
 };
