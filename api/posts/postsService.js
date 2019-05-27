4d76700dd94d0dc5d49d24282abd358d2ebcd98a
GaryChengld <chengld@yahoo.com>
2019-05-26 21:06:46 -0400

    find by id

--- api/posts/postsService.js
+++ api/posts/postsService.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 const Post = mongoose.model('Post');
 
-const post = {
-    title: 'theTitle',
-    content: 'theContent'
-};
-
 const findById = (id) => {
     console.log("find post by id, id=" + id);
     return Post.findById(id);
