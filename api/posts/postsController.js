5bd2ac2cd9bd7af02d4945bab2da756bef17f8da
Gary Cheng <chengld@yahoo.com>
2019-05-28 09:19:40 -0400

    Update postsController.js

--- api/posts/postsController.js
+++ api/posts/postsController.js
@@ -2,11 +2,15 @@ var postsService = require("./postsService");
 
 const findById = (req, res) => {
     postsService.findById(req.params.id)
-        .then(result =>
-            result ? res.status(200).json(result) : res.status(404).json({ "message": "post not found" })
-        )
+        .then(result => result ? res.status(200).json(result) : notFound(res))
         .catch(error => onError(error, res));
 };
+
+const findByAuthor = (req, res) => {
+    postsService.findByAuthor(req.params.author)
+        .then(results => res.status(200).json(results))
+        .catch(error => onError(error, res));
+}
 
 const create = (req, res) => {
     postsService.create(req.body)
@@ -16,19 +20,19 @@ const create = (req, res) => {
 
 const update = (req, res) => {
     postsService.update(req.params.id, req.body)
-        .then(result =>
-            result ? res.status(200).json(result) : res.status(404).json({ "message": "post not found" })
-        )
+        .then(result => result ? res.status(200).json(result) : notFound(res))
         .catch(error => onError(error, res));
 };
 
 const remove = (req, res) => {
     postsService.remove(req.params.id)
-        .then(result =>
-            result ? res.status(204).json(null) : res.status(404).json({ "message": "post not found" })
-        )
+        .then(result => result ? res.status(204).json(null) : notFound(res))
         .catch(error => onError(error, res));
 };
+
+const notFound = (res) => {
+    res.status(404).json({ "message": "post not found" });
+}
 
 const onError = (error, res) => {
     console.log(error);
@@ -36,5 +40,5 @@ const onError = (error, res) => {
 }
 
 module.exports = {
-    findById, create, update, remove
+    findById, findByAuthor, create, update, remove
 };
