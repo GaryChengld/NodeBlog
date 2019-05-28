615be570b1855f13b89c1f91570047e1c644a13c
Gary Cheng <chengld@yahoo.com>
2019-05-28 09:19:10 -0400

    Update postsService.js

--- api/posts/postsService.js
+++ api/posts/postsService.js
@@ -2,29 +2,30 @@ const mongoose = require('mongoose');
 const Post = mongoose.model('Post');
 
 const findById = (id) => {
-    console.log("find post by id, id=" + id);
+    console.log(`find post by id, id=${id}`);
     return Post.findById(id);
 };
 
 const findByAuthor = (author) => {
-    console.log("find post by author, author=" + author);
+    console.log(`find post by author, author=${author}`);
     return Post.find({ author: author })
         .select('id title tags');
 }
 
 const create = (post) => {
+    console.log('create a new post');
     console.log(post);
     return Post.create(post);
 }
 
 const update = (id, post) => {
-    console.log("update post, post id=" + id);
+    console.log(`update a post, post id=${id}`);
     console.log(post);
     return Post.findByIdAndUpdate(id, { $set: { ...post, updatedOn: Date.now() } }, { new: true });
 }
 
 const remove = (id) => {
-    console.log("remove post, post id=" + id);
+    console.log(`remove a post, post id=${id}`);
     return Post.findByIdAndRemove(id);
 }
 
