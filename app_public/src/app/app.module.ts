67ea6093a2752ec815dfce1c03d4c0d6fdff30a1
GaryChengld <chengld@yahoo.com>
2019-06-14 22:24:05 -0400

    nav bar

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponent
+    PostListComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: PostListComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
