c57b7b9ae21fb941bf9e20ec476332161b5f75fc
Gary Cheng <chengld@yahoo.com>
2019-07-04 14:49:35 -0400

    Update authenticationController.js

--- api/users/authenticationController.js
+++ api/users/authenticationController.js
@@ -1,20 +1,37 @@
-var authenticationService = require('./authenticationService');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 const register = (req, res) => {
     if (!req.body.name || !req.body.email || !req.body.password) {
         return res
             .status(400)
             .json({ "message": "All fields required" });
-    } else {
-        authenticationService.register(req.body.name, req.body.email, req.body.password)
-            .then((user) => res.status(200).json({ token: user.generateJwt() }))
-            .catch((error) => onError(error, res));
     }
-}
+
+    const user = new User();
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.setPassword(req.body.password);
+    user.save((err) => {
+        if (err) {
+            onError(err, res);
+        } else {
+            const token = user.generateJwt();
+            res.status(200).json({ token });
+        }
+    })
+};
 
 const onError = (error, res) => {
     console.log(error.message);
-    res.status(error.status || 500).json(error);
+    if (error.code == 11000) {
+        res.status(403).json({ "message": 'User already registered' });
+    } else if (error.message) {
+        res.status(error.status || 500).json({ "message": error.message });
+    } else {
+        res.status(error.status || 500).json(error);
+    }
 }
 
 module.exports = { register };
