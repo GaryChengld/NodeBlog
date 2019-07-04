2d1688ae8eade0713414df89d02a8eb5b212d1a9
Gary Cheng <chengld@yahoo.com>
2019-07-04 15:54:59 -0400

    Update index.js

--- api/users/index.js
+++ api/users/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require('./authenticationController')
 
 router.route('/register').post(authController.register);
+router.route('/login').post(authController.login);
 
 module.exports = router;
