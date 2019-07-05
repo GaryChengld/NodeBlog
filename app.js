2bae1b62298f6ea12d4f20d5485d867a2a0d2f57
Gary Cheng <chengld@yahoo.com>
2019-07-05 15:43:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -19,15 +19,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_public', 'build')));
 app.use(passport.initialize());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
 
-// Routers
+// routers
 require('./api/routes').forEach(router => app.use(router.path, router.handler));
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({ "message": err.name + ": " + err.message });
+  }
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
