be35848ae0e87bc2a7d10904594a8c80a9689837
GaryChengld <chengld@yahoo.com>
2020-12-04 19:45:26 -0500

    reformat

--- api/users/users.schema.js
+++ api/users/users.schema.js
@@ -2,32 +2,31 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    hash: String,
-    salt: String
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  hash: String,
+  salt: String,
 });
 
 userSchema.methods.setPassword = function (password) {
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = createHash(password, this.salt);
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = createHash(password, this.salt);
 };
 
 userSchema.methods.validPassword = function (password) {
-    const hash = createHash(password, this.salt);
-    return this.hash === hash;
+  const hash = createHash(password, this.salt);
+  return this.hash === hash;
 };
 
 const createHash = (password, salt) => {
-    return crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512')
-        .toString('hex');
+  return crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
 };
 
 mongoose.model('User', userSchema);
